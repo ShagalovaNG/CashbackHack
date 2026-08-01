aea21db2df0bbd4735028afc4ec6e11275766d5a
Natalya Shagalova <nataliyshagalyalya@gmail.com>
2026-08-02 01:40:38 +0700

    Branch TestNG

---
+++ src/test/java/CashBackHackerServiceTest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+import ru.netology.services.CashbackHackService;
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CashBackHackerServiceTest {
+    @Test
+    public void CashBackHackerService(){
+        CashbackHackService service =new CashbackHackService();
+        int amount = 1802;
+        int expected =198;
+        int actual=service.remain(amount);
+        assertEquals(actual, expected);
+    }
+}
